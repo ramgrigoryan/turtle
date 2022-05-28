ba657861f59acb605f9a066d7840e585ce1a8692
Vahram <ramgrigoryan@gmail.com>
2022-05-28 17:59:05 +0400

    Preject completed

--- turtle.js
+++ turtle.js
@@ -61,24 +61,91 @@
 // gradient("n");
 
 //Task 32
-function triangleShuriken() {
+// function triangleShuriken() {
+//   let length = 50;
+//   let delta = 5;
+//   left(180);
+//   for (let i = 0; i < 200; i++) {
+//     i % 3 == 2
+//       ? colour(60, 179, 113, 1)
+//       : i % 3 == 1
+//       ? colour(218, 112, 214, 1)
+//       : colour(220, 20, 60, 1);
+//     if (i % 2 == 0) {
+//       left(1 + Math.PI / 360);
+//       length += delta;
+//       forward(length);
+//     } else {
+//       forward(length);
+//     }
+//     left(120);
+//   }
+// }
+// triangleShuriken();
+
+//Task 33
+
+const randomNumber = (from, to) => Math.round((to - from) * Math.random());
+
+// function drawColoredLine(length) {
+//   for (let i = 0; i < 10; i++) {
+//     colour(0, randomNumber(0, 255), randomNumber(0, 255), 1);
+//     forward(length/10);
+//   }
+// }
+function drawColoredLine(length) {
+  for (let i = 0; i < 10; i++) {
+    switch (randomNumber(1, 4)) {
+      case 1:
+        colour(173, 216, 230, 1);
+        break;
+      case 2:
+        colour(216, 191, 216, 1);
+        break;
+      case 3:
+        colour(220, 220, 220, 1);
+        break;
+      default:
+        colour(70, 130, 180, 1);
+    }
+    forward(length / 10);
+  }
+}
+
+// const randomIntNumber = (from, to) => Math.round((to - from) * Math.random());
+
+// function drawColoredLine(length) {
+//   for (let i = 0; i < 10; i++) {
+//     switch (randomIntNumber(1, 4)) {
+//       case 1:
+//         colour(173, 216, 230);
+//         break;
+//       case 2:
+//         colour(216, 191, 216);
+//         break;
+//       case 3:
+//         colour(220, 220, 220);
+//         break;
+//       default:
+//         colour(70, 130, 180);
+//     }
+//     forward(length / 10);
+//   }
+// }
+
+function triangleChameleonShuriken() {
   let length = 50;
   let delta = 5;
-  left(180);
+  left(45);
   for (let i = 0; i < 200; i++) {
-    i % 3 == 2
-      ? colour(60, 179, 113, 1)
-      : i % 3 == 1
-      ? colour(218, 112, 214, 1)
-      : colour(220, 20, 60, 1);
     if (i % 2 == 0) {
-      left(1 + Math.PI / 360);
+      left(1.5 + Math.PI / 90);
       length += delta;
-      forward(length);
+      drawColoredLine(length);
     } else {
-      forward(length);
+      drawColoredLine(length);
     }
     left(120);
   }
 }
-triangleShuriken();
+triangleChameleonShuriken();
